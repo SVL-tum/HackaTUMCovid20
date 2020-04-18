cee9d4abe27a10a967141c16393c760eaf75d319
SveaMeyer13 <svea.meyer@tum.de>
2020-04-18 23:55:34 +0200

    add super ugly hack to get data every second

--- Hackathon/app.py
+++ Hackathon/app.py
@@ -1,17 +1,20 @@
+import copy
 import sys
 from datetime import date, time, timedelta
 
 from flask import Flask, request
 import json
 from time import sleep
+import time
 import requests
 from Hackathon import Patient
 from Hackathon import VentilatorOld as Ventilator
 from collections import OrderedDict
 from _thread import start_new_thread
 from datetime import datetime
-#link to communicate externally: http://129.187.212.1:5000/patient?sorting=rscore
-#Link to communicate internally: curl localhost:5001/data?"patientid=1000&seconds=300&measurement=O2"
+
+# link to communicate externally: http://129.187.212.1:5000/patient?sorting=rscore
+# Link to communicate internally: curl localhost:5001/data?"patientid=1000&seconds=300&measurement=O2"
 
 app = Flask(__name__)
 
@@ -21,6 +24,7 @@ x = dir(Ventilator)
 print(x)
 
 patients = Patient.get_patients()
+
 
 ## ?
 def serialize(obj):
@@ -44,6 +48,7 @@ def serialize(obj):
 
     return obj.__dict__
 
+
 ## get ventilators
 ventilator_ids = [4242, 3089, 12693, 93, 586]
 ventilators = []
@@ -51,6 +56,8 @@ ventilators = []
 for venid in ventilator_ids:
     v = Ventilator.Ventilator(venid)
     ventilators.append(v)
+
+
 #
 # for i in range(10):
 #     sleep(0.5)
@@ -61,29 +68,36 @@ for venid in ventilator_ids:
 #             if data[key]['device_id'] == v.id:
 #                 v.add_data(data[key])
 
-def continous_update_of_ventilators():
-    while 1==1:
 
-      sleep(0.5)
-      try:
-          r = requests.get('https://api.theopenvent.com/exampledata/v2/data', verify=False)
-          data = r.json()
-          for key in data:
-             for v in ventilators:
-                if data[key]['device_id'] == v.id:
+## super ugly hack
+def continous_update_of_ventilators():
+    timeout = 600
+    while 1 == 1:
+        sleep(0.2)
+        try:
+            r = requests.get('https://api.theopenvent.com/exampledata/v2/data', verify=False, timeout=timeout)
+            timeout = 0.6
+            data = r.json()
+            for key in data:
+                for v in ventilators:
+                    if data[key]['device_id'] == v.id:
                         v.add_data(data[key])
-      except Exception as ex:
-          print(ex)
+        except Exception as ex:
+            print(ex)
+            for v in ventilators:
+                last = v.get_last_seconds(1)[0]
+                for diff in [-1,0,1]:
+                    current = copy.deepcopy(last)
+                    current["time"] = int(time.time()+diff)
+                    v.add_data(current)
 
 start_new_thread(continous_update_of_ventilators, ())
-
 
 map_patients_to_ventilator = OrderedDict()
 map_ventilators_to_patients = OrderedDict()
 for idx, p in enumerate(patients):
     map_patients_to_ventilator[p] = ventilators[idx]
     map_ventilators_to_patients[ventilators[idx]] = p
-
 
 
 @app.route('/patient')
@@ -107,10 +121,11 @@ def get_patient_list():
 
     return json.dumps(patients_new, default=serialize)
 
-   # user = request.args.get('name')
 
-   #
-    #return patients
+# user = request.args.get('name')
+
+#
+# return patients
 
 @app.route('/tdata')
 def get_tventilator_data():
@@ -125,20 +140,22 @@ def get_tventilator_data():
         if p.id == patientid:
             all_about_ventilator = map_patients_to_ventilator[p].get_last_seconds(seconds)
     if all_about_ventilator is None:
-        return "Patient "+str(patientid)+" not found"
+        return "Patient " + str(patientid) + " not found"
     timestamps = []
     for timepoints in all_about_ventilator:
         timestamps.append(timepoints['time'])
         if measurement == 'O2':
-           ventilator_for_chris.append(float(timepoints['raw']['O2']))
+            ventilator_for_chris.append(float(timepoints['raw']['O2']))
         elif measurement == 'MVe':
-           ventilator_for_chris.append(float(timepoints['processed']['MVe']))
+            ventilator_for_chris.append(float(timepoints['processed']['MVe']))
         elif measurement == 'Co2':
             ventilator_for_chris.append(float(timepoints['raw']['CO2']))
         elif measurement == 'frequency':
             ventilator_for_chris.append(float(timepoints['processed']['frequency']))
-        else: return "Measure "+measurement+" not supported."
+        else:
+            return "Measure " + measurement + " not supported."
     return json.dumps({"timestamps": timestamps, "measurements": ventilator_for_chris})
+
 
 @app.route('/data')
 def get_ventilator_data():
@@ -153,16 +170,18 @@ def get_ventilator_data():
         if p.id == patientid:
             all_about_ventilator = map_patients_to_ventilator[p].get_last_seconds(seconds)
     if all_about_ventilator is None:
-        return "Patient "+str(patientid)+" not found"
+        return "Patient " + str(patientid) + " not found"
     for timepoints in all_about_ventilator:
         if measurement == 'O2':
-           ventilator_for_chris.append(timepoints['raw']['O2'])
+            ventilator_for_chris.append(timepoints['raw']['O2'])
         elif measurement == 'MVe':
-           ventilator_for_chris.append(float(timepoints['processed']['MVe']))
+            ventilator_for_chris.append(float(timepoints['processed']['MVe']))
         elif measurement == 'Co2':
             ventilator_for_chris.append(float(timepoints['raw']['CO2']))
-        else: return "Measure "+measurement+" not supported."
+        else:
+            return "Measure " + measurement + " not supported."
     return json.dumps(ventilator_for_chris)
+
 
 @app.route('/patient_by_id')
 def get_patient_by_id():
@@ -198,13 +217,14 @@ def change_state():
             p.set_delta()
     return "changed state"
 
+
 ## how to run in terminal
 # export PYTHONPATH="/home/svea/Desktop/Wo_der_server_lauft/HackaTUMCovid20/"
 # source ./Hackathon/venv/bin/activate
 # python Hackathon/app.py
 
 # Does not execute this
-#From terminal, we need export PYTHONPATH="/home/svea/Desktop/HackaTUMCovid20;/home/svea/Desktop/HackaTUMCovid20/Hackathon"
+# From terminal, we need export PYTHONPATH="/home/svea/Desktop/HackaTUMCovid20;/home/svea/Desktop/HackaTUMCovid20/Hackathon"
 
 if __name__ == '__main__':
     args = sys.argv
@@ -212,4 +232,3 @@ if __name__ == '__main__':
         app.run(host='0.0.0.0', port=5001)
     else:
         app.run(host='0.0.0.0')
-
